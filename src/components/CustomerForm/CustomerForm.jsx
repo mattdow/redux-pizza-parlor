0ed3ea2bf886a99d5ccdb8bab7644cd56fd39bfb
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-28 14:31:58 -0500

    sets newCustomer as object, radio buttons

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -3,57 +3,92 @@ import { useState } from 'react';
 // import Picker from 'react-picker';
 
 function CustomerForm() {
+  const [newCustomer, setNewCustomer] = useState({
+    customer_name: '',
+    street_address: '',
+    city: '',
+    zip: '',
+    type: '',
+  });
 
-    const [newCustomer, setNewCustomer] = useState('');
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-        dispatch({
-            type: 'ADD_CUSTOMER',
-            payload: newCustomer
-        })
+    dispatch({
+      type: 'SET_CUSTOMER_INFO',
+      payload: newCustomer,
+    });
 
-        setNewCustomer('');
-    }
+    setNewCustomer({
+      customer_name: '',
+      street_address: '',
+      city: '',
+      zip: '',
+      type: '',
+    });
+  };
 
-    return (
+  console.log(`newCustomer`, newCustomer);
 
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Step 2: Customer Information</h2>
+      <label>PickUp or Delivery</label>
+      <input
+        type="text"
+        placeholder="Name"
+        value={newCustomer.customer_name}
+        onChange={(event) =>
+          setNewCustomer({ ...newCustomer, customer_name: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        placeholder="Street Address"
+        value={newCustomer.street_address}
+        onChange={(event) =>
+          setNewCustomer({ ...newCustomer, street_address: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        placeholder="City"
+        value={newCustomer.city}
+        onChange={(event) =>
+          setNewCustomer({ ...newCustomer, city: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        placeholder="Zip"
+        value={newCustomer.zip}
+        onChange={(event) =>
+          setNewCustomer({ ...newCustomer, zip: event.target.value })
+        }
+      />
 
-        <form onSubmit={handleSubmit}>
-            <h2>Step 2: Customer Information</h2>
-            <label>PickUp or Delivery</label>
-            <input
-            type ="text"
-            placeholder="Name"
-            Value={newCustomer}
-            onChange={event => setNewCustomer(event.target.value)}
-            />
-            <input
-            type ="text"
-            placeholder="Street Address"
-            Value={newCustomer}
-            onChange={event => setNewCustomer(event.target.value)}
-            />
-            <input
-            type ="text"
-            placeholder="City"
-            Value={newCustomer}
-            onChange={event => setNewCustomer(event.target.value)}
-            />
-            <input
-            type ="text"
-            placeholder="Zip"
-            Value={newCustomer}
-            onChange={event => setNewCustomer(event.target.value)}
-            />
-            <button>Pick Up</button>
-            <button>Delivery</button>
+      <input
+        type="radio"
+        name="type"
+        value="Pickup"
+        id="pickup"
+        onClick={() => setNewCustomer({ ...newCustomer, type: 'Pickup' })}
+      />
+      <label htmlFor="pickup">Pick up</label>
+      <input
+        type="radio"
+        name="type"
+        value="Delivery"
+        id="delivery"
+        onClick={() => setNewCustomer({ ...newCustomer, type: 'Delivery' })}
+      />
+      <label htmlFor="delivery">Delivery</label>
 
-            <button type="next">NEXT</button>
-        </form>
-    )
+      <button type="submit">NEXT</button>
+    </form>
+  );
 }
 
 export default CustomerForm;
