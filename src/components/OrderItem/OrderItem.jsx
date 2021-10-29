1add2707f93b755ed3dbbaf2d3c65a0b762ed857
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-29 10:15:14 -0500

    adds OrderItem component (to access id) on Admin page

---
+++ src/components/OrderItem/OrderItem.jsx
@@ -0,0 +1,40 @@
+function OrderItem({ order }) {
+  const convertTime = (time) => {
+    // convert the time from the server into a js object
+    const t = new Date(time);
+    // the month is 0-indexed, so we need to add 1!
+    let month = t.getMonth() + 1;
+    let dayInMonth = t.getDate();
+    let year = t.getFullYear();
+    // change the hours from military time to am / pm
+    let hours;
+    let amOrPm = 'am';
+    hours = t.getHours();
+    if (hours > 12) {
+      // set this to am and pm time
+      hours = hours - 12;
+      // in this case it's pm
+      amOrPm = 'pm';
+    }
+    // set the minutes to display correctly, adding a 0 if less than 10
+    let minutes = String(t.getMinutes());
+    if (minutes.length === 1) {
+      // add a zero in front if the length of minutes, as a string, is 1
+      minutes = '0' + minutes;
+    }
+    // return all of the above in a nice string using template literals
+    return `${month}/${dayInMonth}/${year} at ${hours}:${minutes}${amOrPm} `;
+  };
+
+  console.log('display?');
+  return (
+    <tr id={order.id}>
+      <td>{order.customer_name}</td>
+      <td>{convertTime(order.time)}</td>
+      <td>{order.type}</td>
+      <td>{order.total}</td>
+    </tr>
+  );
+}
+
+export default OrderItem;
