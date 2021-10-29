bab80c9dcb04a4f394c09e2f0d4470c04ebf7a0d
Chaoching Vang <chaoching.vang@gmail.com>
2021-10-29 10:41:44 -0500

    created back btn and footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import { useHistory } from "react-router";
+import './Footer.css';
+
+function Footer({page}) {
+
+
+    return (<>
+        <div className="Footer">
+            <p><span> &copy;</span>Chaoching Vang, Jacob Birkeland, Matt Dow & Ronald van der Bergh</p>
+        </div>
+    </>)
+}
+
+export default Footer;
