bd361ad56b89118cb60d4422500b81a26220d7c5
Matt Dow <matt.dow.vo@gmail.com>
2021-10-29 10:13:48 -0500

    Imported cards and typography

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -2,6 +2,13 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './PizzaItem.css';
 import { useSelector, useDispatch } from 'react-redux';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 function PizzaItem({ item }) {
   // define a variable equal to the current order number
@@ -50,20 +57,59 @@ function PizzaItem({ item }) {
 
   return (
     <div className="gallery-item">
-      <img src={item.image_path} alt={item.name} width="150" height="150" />
-      <h2 className="pizza-name">{item.name}</h2>
-      <p className="pizza-description">{item.description}</p>
-      <p className="pizza-price">{item.price}</p>
-      {addDelete ? (
-        <button className="add-btn" onClick={addAndToggle}>
+    <Card variant="outlined" sx={{ maxWidth: 200 }}>  
+          <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="150"
+                    width="150"
+                    image={item.image_path}
+                />
+                <CardContent>
+                    <Typography className="pizza-name" gutterBottom variant="h5" color="GrayText.secondary">
+                        {item.name}
+                    </Typography>
+                    <Typography className="pizza-description" gutterBottom variant="body2" color="GrayText.secondary">
+                        {item.description}
+                    </Typography>
+                    <Typography className="pizza-price" gutterBottom variant="body2" color="GrayText.secondary">
+                        ${item.price}
+                    </Typography>
+
+                </CardContent>
+          </CardActionArea>
+          
+            
+            {/* Button to delete card */}
+            {addDelete ? (
+        <Button size="small" color="primary" onClick={addAndToggle}>
           ADD
-        </button>
+        </Button>
       ) : (
-        <button className="delete-btn" onClick={deleteAndToggle}>
+        <Button className="delete-btn" onClick={deleteAndToggle}>
           DELETE
-        </button>
+        </Button>
       )}
-    </div> // end gallery item div
+            
+           
+    </Card>
+    </div>
+
+    // <div className="gallery-item">
+    //   <img src={item.image_path} alt={item.name} width="150" height="150" />
+    //   <h2 className="pizza-name">{item.name}</h2>
+    //   <p className="pizza-description">{item.description}</p>
+    //   <p className="pizza-price">{item.price}</p>
+    //   {addDelete ? (
+    //     <button className="add-btn" onClick={addAndToggle}>
+    //       ADD
+    //     </button>
+    //   ) : (
+    //     <button className="delete-btn" onClick={deleteAndToggle}>
+    //       DELETE
+    //     </button>
+    //   )}
+    // </div> // end gallery item div
   );
 } // end of PizzaItem function
 
